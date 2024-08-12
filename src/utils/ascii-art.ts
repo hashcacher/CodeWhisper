12394506b3397d3024a6ce89bc133fd7dba54eaa
Greg Guterman <gregoryguterman@gmail.com>
2024-08-12 08:34:12 -0700

    CodeWhisper: Add kitten ASCII art with raised left paws to generated pull requests

--- src/utils/ascii-art.ts
+++ src/utils/ascii-art.ts
@@ -1,29 +1,15 @@
 export function generateKittenAsciiArt(): string {
-  const kittenArts = [
-    `
- /\\_/\\
-( o.o )
- > ^ <
-    `,
-    `
- |\\__/,|   (\`\\
- |_ _  |.--.) )
- ( T   )     /
-(((^_(((/(((_/
-    `,
-    `
-    |\\      _,,,---,,_
-    /,\`.-'\`'    -.  ;-;;,_
-   |,4-  ) )-,_..;\\ (  \`'-'
-  '---''(_/--'  \`-'\\_)
-    `,
-    `
-     |\\__/,|   (\`\\
-   _.|o o  |_   ) )
- -(((---(((--------
-    `
-  ];
-
-  const randomIndex = Math.floor(Math.random() * kittenArts.length);
-  return kittenArts[randomIndex].trim();
+  return `
+   /\\___/\\
+  ( o   o )
+  (  =^=  )
+  (        )
+  (         )
+ (           )
+(  /\\     /\\  )
+ (  |\\_/|  )
+  (  |   |  )
+   (/ \\_/ \\)
+    \\_____/
+  `;
 }
